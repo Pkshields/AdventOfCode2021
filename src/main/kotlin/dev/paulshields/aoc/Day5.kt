509a7bcc95ab31c1dafe25f5f95a4e99d0d9709f
Paul Shields <pkshields@gmail.com>
2021-12-05 22:42:50 +0000

    Adding solution for Day 5: Hydrothermal Venture

---
+++ src/main/kotlin/dev/paulshields/aoc/Day5.kt
@@ -0,0 +1,58 @@
+/**
+ * Day 5: Hydrothermal Venture
+ */
+
+package dev.paulshields.aoc
+
+import dev.paulshields.aoc.common.readFileAsStringList
+
+fun main() {
+    println(" ** Day 5: Hydrothermal Venture ** \n")
+
+    val rawHydrothermalVents = readFileAsStringList("/Day5HydrothermalVentsLineSegments.txt")
+
+    val hydrothermalVents = rawHydrothermalVents.map {
+        it.split(" -> ").map {
+            it.split(",")
+                .mapNotNull { it.toIntOrNull() }
+                .let(Point::fromList)
+        }.let(LineSegment::fromList)
+    }
+
+    val ventsExcludingDiagonals = hydrothermalVents.filter { it.start.x == it.end.x || it.start.y == it.end.y }
+
+    val numberOfOverlapPointExcludingDiagonals = countPointsWhereLinesOverlap(ventsExcludingDiagonals)
+    println("There are $numberOfOverlapPointExcludingDiagonals points where the hydrothermal vents overlap excluding diagonals")
+
+    val numberOfOverlapPoints = countPointsWhereLinesOverlap(hydrothermalVents)
+    println("There are $numberOfOverlapPoints points where the hydrothermal vents overlap including diagonals")
+}
+
+fun countPointsWhereLinesOverlap(lineSegments: List<LineSegment>) =
+    lineSegments.flatMap { it.pointsOnLine }
+        .groupingBy { it }
+        .eachCount()
+        .filter { it.value > 1 }
+        .count()
+
+data class Point(val x: Int, val y: Int) {
+    companion object {
+        fun fromList(xy: List<Int>) = Point(xy.component1(), xy.component2())
+    }
+}
+
+data class LineSegment(val start: Point, val end: Point) {
+    companion object {
+        fun fromList(xy: List<Point>) = LineSegment(xy.component1(), xy.component2())
+    }
+
+    val pointsOnLine = if (start.x == end.x) {
+        directionlessRange(start.y, end.y).map { Point(start.x, it) }
+    } else if (start.y == end.y) {
+        directionlessRange(start.x, end.x).map { Point(it, start.y) }
+    } else {
+        directionlessRange(start.x, end.x).zip(directionlessRange(start.y, end.y)).map { Point(it.first, it.second) }
+    }
+}
+
+private fun directionlessRange(start: Int, end: Int) = if (start < end) (start..end) else (start downTo end)
---
+++ src/test/kotlin/dev/paulshields/aoc/Day5Test.kt
@@ -0,0 +1,85 @@
+package dev.paulshields.aoc
+
+import assertk.assertThat
+import assertk.assertions.containsExactly
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+class Day5Test {
+    @Test
+    fun `should correctly count the points where lines overlap without any diagonals`() {
+        val lineSegments = listOf(
+            LineSegment(Point(0, 9), Point(5, 9)),
+            LineSegment(Point(9, 4), Point(3, 4)),
+            LineSegment(Point(2, 2), Point(2, 1)),
+            LineSegment(Point(7, 0), Point(7, 4)),
+            LineSegment(Point(0, 9), Point(2, 9)),
+            LineSegment(Point(3, 4), Point(1, 4)))
+
+        val result = countPointsWhereLinesOverlap(lineSegments)
+
+        assertThat(result).isEqualTo(5)
+    }
+
+    @Test
+    fun `should correctly count the points where lines overlap with any diagonals`() {
+        val lineSegments = listOf(
+            LineSegment(Point(0, 9), Point(5, 9)),
+            LineSegment(Point(8, 0), Point(0, 8)),
+            LineSegment(Point(9, 4), Point(3, 4)),
+            LineSegment(Point(2, 2), Point(2, 1)),
+            LineSegment(Point(7, 0), Point(7, 4)),
+            LineSegment(Point(6, 4), Point(2, 0)),
+            LineSegment(Point(0, 9), Point(2, 9)),
+            LineSegment(Point(3, 4), Point(1, 4)),
+            LineSegment(Point(0, 0), Point(8, 8)),
+            LineSegment(Point(5, 5), Point(8, 2)))
+
+        val result = countPointsWhereLinesOverlap(lineSegments)
+
+        assertThat(result).isEqualTo(12)
+    }
+
+    @Nested
+    inner class LineSegmentTest {
+        @Test
+        fun `should correctly calculate all points on a line segment for a horizontal line`() {
+            val target = LineSegment(Point(0, 9), Point(5, 9))
+
+            assertThat(target.pointsOnLine).containsExactly(
+                Point(0, 9),
+                Point(1, 9),
+                Point(2, 9),
+                Point(3, 9),
+                Point(4, 9),
+                Point(5, 9))
+        }
+
+        @Test
+        fun `should correctly calculate all points on a line segment for a vertical line`() {
+            val target = LineSegment(Point(6, 0), Point(6, 5))
+
+            assertThat(target.pointsOnLine).containsExactly(
+                Point(6, 0),
+                Point(6, 1),
+                Point(6, 2),
+                Point(6, 3),
+                Point(6, 4),
+                Point(6, 5))
+        }
+
+        @Test
+        fun `should correctly calculate all points on a line segment for a diagonal line`() {
+            val target = LineSegment(Point(0, 0), Point(5, 5))
+
+            assertThat(target.pointsOnLine).containsExactly(
+                Point(0, 0),
+                Point(1, 1),
+                Point(2, 2),
+                Point(3, 3),
+                Point(4, 4),
+                Point(5, 5))
+        }
+    }
+}
